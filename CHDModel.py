28a675c65a7a48321542a2df3e8609069a18f253
Matthew Kanderka <kanderkam@hotmail.com>
2019-12-01 14:35:43 -0700

    initial model for chd

--- CHDModel.py
+++ CHDModel.py
@@ -5,46 +5,86 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from tensorflow_core.python.keras import regularizers
+from numpy.random import RandomState
 
-heart_file = pd.read_csv("heart.csv")
-heart_file.head()
+LABEL_COLUMN = "chd"
+TRAIN_DATA_PATH = "heart_train.csv"
+TEST_DATA_PATH = "heart_test.csv"
 
-train, test = train_test_split(heart_file, test_size=0.2)
-train, val = train_test_split(train, test_size=0.2)
+df = pd.read_csv("heart.csv")
+rng = RandomState()
 
-def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+train = df.sample(frac=0.7, random_state=rng)
+test = df.loc[~df.index.isin(train.index)]
+
+train.to_csv("heart_train.csv", index=False)
+test.to_csv("heart_test.csv", index=False)
+
+class PackNumericFeatures(object):
+  def __init__(self, names):
+    self.names = names
+
+  def __call__(self, features, labels):
+    numeric_features = [features.pop(name) for name in self.names]
+    numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
+    numeric_features = tf.stack(numeric_features, axis=-1)
+    features['numeric'] = numeric_features
+
+    return features, labels
+
+def normalize_numeric_data(data, mean, std):
+  # Center the data
+  return (data-mean)/std
+
+def get_dataset(file_path, **kwargs):
+    dataset = tf.data.experimental.make_csv_dataset(file_path, batch_size=5, label_name=LABEL_COLUMN)
+    return dataset
+
+raw_train_data = get_dataset(TRAIN_DATA_PATH)
+raw_test_data = get_dataset(TEST_DATA_PATH)
+
+NUMERIC_FEATURES = ['sbp', 'tobacco', 'ldl', 'adiposity', 'typea','obesity','alcohol','age']
+
+packed_train_data = raw_train_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+packed_test_data = raw_test_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+
+# normalize data
+desc = pd.read_csv(TRAIN_DATA_PATH)[NUMERIC_FEATURES].describe()
+
+MEAN = np.array(desc.T['mean'])
+STD = np.array(desc.T['std'])
+
+normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
+numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
+numeric_columns = [numeric_column]
+
+CATEGORIES = {
+    'famhist': ['Present', 'Absent']
+}
+
+categorical_columns = []
+for feature, vocab in CATEGORIES.items():
+    cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+        key=feature, vocabulary_list=vocab)
+    categorical_columns.append(tf.feature_column.indicator_column(cat_col))
+
+preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns+numeric_columns)
+
+def create_ds(dataframe, batch_size=1):
     dataframe = dataframe.copy()
     labels = dataframe.pop('chd')
-    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-    if shuffle:
-        ds = ds.shuffle(buffer_size=len(dataframe))
-    ds = ds.batch(batch_size)
-    return ds
-
-batch_size = 5
-train_ds = df_to_dataset(train, batch_size=batch_size)
-val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-feature_columns = []
-
-# numeric cols
-for header in ['sbp', 'tobacco', 'ldl', 'adiposity', 'typea','obesity','alcohol','age']:
-    feature_columns.append(tf.feature_column.numeric_column(header))
-
-# indicator cols
-famhist = tf.feature_column.categorical_column_with_vocabulary_list(
-    'famhist', ['Present', 'Absent'])
-famhist_one = tf.feature_column.indicator_column(famhist)
-feature_columns.append(famhist_one)
+    return tf.data.Dataset.from_tensor_slices((dict(dataframe), labels)).shuffle(buffer_size=len(dataframe)).batch(batch_size)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.DenseFeatures(feature_columns),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1, activation='sigmoid'),
+    preprocessing_layer,
+    tf.keras.layers.Dense(256, kernel_regularizer=regularizers.l2(0.001), activation = 'relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(128, kernel_regularizer=regularizers.l2(0.001), activation = 'relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(64, kernel_regularizer=regularizers.l2(0.001), activation = 'relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1, activation='sigmoid')
     ])
     
 
@@ -53,10 +93,10 @@ model.compile(optimizer='adam',
                 metrics=['accuracy'])
 
 print("--Fit model--")
-model.fit(train_ds, epochs=10)
+model.fit(packed_train_data, epochs=20, steps_per_epoch=128)
 
 print("--Evaluate model--")
-model_loss, model_acc = model.evaluate(test_ds, verbose=2)
+model_loss, model_acc = model.evaluate(packed_test_data, verbose=2, steps=128)
 print(f"Model Loss:    {model_loss:.2f}")
 print(f"Model Accuray: {model_acc*100:.1f}%")
 
